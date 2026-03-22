6c0c39d3bd9eec607279f752f6fd9282da835f59
programmeur06 <groupesessinoubusiness@gmail.com>
2026-03-22 15:46:44 +0100

    chang

--- src/public/commande-mode/mode-order.controller.ts
+++ src/public/commande-mode/mode-order.controller.ts
@@ -2,7 +2,7 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { CreateModeOrderDto } from './dto/create-mode-order.dto';
 import { ModeOrderService } from './mode-order.service';
 
-@Controller('mode-request')
+@Controller('public/mode-request')
 export class ModeOrderController {
   constructor(private readonly service: ModeOrderService) {}
 
